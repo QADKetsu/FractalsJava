8542780ac191cfe99a45d279ad20b583e6fd96de
Jovian Lim <kiriketsuki@gmail.com>
2021-11-30 02:52:18 +0800

    learnt how to make custom colour maps

--- src/Colours/Generated.java
+++ src/Colours/Generated.java
@@ -1,5 +1,33 @@
 package Colours;
+import java.awt.Color;
+import java.util.*;
+import MathHelper.MonotoneCubicSpline;
 
 public class Generated {
-    
+    // uses cubic interpolation to generate a colour map
+
+    public static Color[] firstMap() {
+        // this shall be white blue black 
+        Color[] colourMap = new Color[256];
+        // i will use HSV for the interpolation
+        double h = 191.0/360.0;
+        Double[] sArray = new Double[] {0.0, 0.15, 0.33, 0.5, 0.65, 0.84, 1.0};
+        List<Double> sList = Arrays.asList(sArray);
+        Double[] vArray = new Double[] {1.0, 0.86, 0.70, 0.56, 0.48, 0.30, 0.0};
+        List<Double> vList = Arrays.asList(vArray);
+        // interpolate s first
+        Double[] xArray = new Double[] {(double) 0,(double) 1/6,(double) 2/6,(double) 3/6,(double) 4/6,(double) 5/6,(double) 6/6};
+        List<Double> xList = Arrays.asList(xArray); 
+
+        MonotoneCubicSpline sSpline = MonotoneCubicSpline.createMonotoneCubicSpline(xList, sList);
+        MonotoneCubicSpline vSpline = MonotoneCubicSpline.createMonotoneCubicSpline(xList, vList);
+
+        for (int i = 0; i < 256; i++) {
+            double s = sSpline.interpolate(i/255.0);
+            double v = vSpline.interpolate(i/255.0);
+            
+            colourMap[i] = Color.getHSBColor((float) h, (float) s, (float) v);
+        }
+        return colourMap;
+    }
 }
--- src/Mandelbrot/SmoothHistOptPanel.java
+++ src/Mandelbrot/SmoothHistOptPanel.java
@@ -3,11 +3,13 @@ package Mandelbrot;
 import MathHelper.LinearMapping;
 import java.awt.*;
 
+import Colours.Generated;
 import Colours.Hardcoded;
 
 
 public class SmoothHistOptPanel extends MandelbrotPanel {
-
+    private Color[] map;
+    
     public SmoothHistOptPanel(int imageWidth, int imageHeight) {
         super(imageWidth, imageHeight);
     }
@@ -19,6 +21,7 @@ public class SmoothHistOptPanel extends MandelbrotPanel {
             smoothHistOptCalc = new SmoothHistOptCalc(width, height);
             super.calc = smoothHistOptCalc;
             System.out.println("Smoothed Histogram Optimized Mandelbrot created");
+            map = Generated.firstMap();
         } else {
             smoothHistOptCalc = super.calc;
         }
@@ -39,45 +42,48 @@ public class SmoothHistOptPanel extends MandelbrotPanel {
             for (int y = 0; y < height; y++) {
                 double hueVal = hue[x][y];
                 int colourVal;
-                Color[] map = Hardcoded.firstMap();
-                switch (colourVersion % 5) {
-                    case 0:
-                        colourVal = (int) LinearMapping.map(hueVal, 0, 1, 0, 255);
-                        fractalImage.setRGB(x, y, new Color(colourVal, colourVal, colourVal).getRGB());
-                        break;
-                    case 1:
-                        if (hueVal == 0) {
-                            fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
-                        } else {
-                            colourVal = (int) LinearMapping.map(hueVal, 0, 1, 15, 0);
-                            fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
-                        }
-                        break;
-                    case 2:
-                        if (hueVal == 0) {
-                            fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
-                        } else {
-                            colourVal = (int) LinearMapping.map(hueVal, 0, 1, 19, 4);
-                            fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
-                        }
-                        break;
-                    case 3:
-                        if (hueVal == 0) {
-                            fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
-                        } else {
-                            colourVal = (int) LinearMapping.map(hueVal, 0, 1, 23, 8);
-                            fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
-                        }
-                        break;
-                    case 4:
-                        if (hueVal == 0) {
-                            fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
-                        } else {
-                            colourVal = (int) LinearMapping.map(hueVal, 0, 1, 27, 12);
-                            fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
-                        }
-                        break;
-                }
+                // Color[] map = Hardcoded.firstMap();
+                // switch (colourVersion % 5) {
+                //     case 0:
+                //         colourVal = (int) LinearMapping.map(hueVal, 0, 1, 0, 255);
+                //         fractalImage.setRGB(x, y, new Color(colourVal, colourVal, colourVal).getRGB());
+                //         break;
+                //     case 1:
+                //         if (hueVal == 0) {
+                //             fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
+                //         } else {
+                //             colourVal = (int) LinearMapping.map(hueVal, 0, 1, 15, 0);
+                //             fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
+                //         }
+                //         break;
+                //     case 2:
+                //         if (hueVal == 0) {
+                //             fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
+                //         } else {
+                //             colourVal = (int) LinearMapping.map(hueVal, 0, 1, 19, 4);
+                //             fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
+                //         }
+                //         break;
+                //     case 3:
+                //         if (hueVal == 0) {
+                //             fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
+                //         } else {
+                //             colourVal = (int) LinearMapping.map(hueVal, 0, 1, 23, 8);
+                //             fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
+                //         }
+                //         break;
+                //     case 4:
+                //         if (hueVal == 0) {
+                //             fractalImage.setRGB(x, y, new Color(0, 0, 0).getRGB());
+                //         } else {
+                //             colourVal = (int) LinearMapping.map(hueVal, 0, 1, 27, 12);
+                //             fractalImage.setRGB(x, y, map[colourVal%16].getRGB());
+                //         }
+                //         break;
+
+                // }
+                colourVal = (int) LinearMapping.map(hueVal, 0, 1, 255, 0);
+                fractalImage.setRGB(x, y, map[colourVal].getRGB());
             }
         }
     }
